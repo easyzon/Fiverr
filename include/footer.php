f115e655d3ed5dd22618cbed6b6ebae01fc4204b
adeel0098 <110596492+adeel0098@users.noreply.github.com>
2023-04-04 12:59:20 +0500

    new update form

--- include/footer.php
+++ include/footer.php
@@ -124,12 +124,13 @@
       </div>
     </div>
   </footer>
+<script>
 
-
-  <!-- custom scripts -->
+ 
+  // <!-- custom scripts -->
   <script src="./assets/utils/content.js"></script>
   <script src="./assets/script.js"></script>
-  <!-- bootstrap  -->
+  // <!-- bootstrap  -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -137,4 +138,20 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
   </body>
 
+  // <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/642bc14d31ebfa0fe7f65ac5/1gt5em89m';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+// <!--End of Tawk.to Script-->
+</script>
+
 </html>
+
